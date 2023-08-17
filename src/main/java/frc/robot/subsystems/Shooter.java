fc76eac706d7460525981c5cfb8869062dc3dc30
braykoff <99614905+Braykoff@users.noreply.github.com>
2023-08-17 15:26:24 -0400

    Updated shooter instructions

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -13,15 +13,16 @@ public class Shooter extends SubsystemBase {
      * The shooter is made of two parts:
      * - The shooter, controlled by two CanSparkMax's that are connected
      * - The indexer, which feeds balls to the shooter, and is controlled by a
-     *      single Falcon500
+     *      single Falcon500 (TalonFX!)
      * 
      * All the ShooterConstants and HardwareConstants are already set up.
      * For this file, you need to:
      * - Create two CanSparkMax's (use HardwareConstants for ID's)
      * - Create one TalonFX
-     * - Create a method to set the speed of the CanSparkMax
+     * - Create a method to set the speed of the CanSparkMax (leave this blank 
+     *      for now)
      * - Create a method that turns on/off the TalonFX (use the speed from 
-     *  ShooterConstants)
+     *      ShooterConstants)
      * 
      * You will need to do configuration for the shooter motor. Remember, they
      * are connected physically, so they need to spin together. See:
@@ -30,5 +31,8 @@ public class Shooter extends SubsystemBase {
      * Don't worry about the PID values yet, we'll go over this later.
      * Feel free to look at the other two subsystems as a reference for setting
      * up motors and methods. Remember what needs to be public or private!
+     * 
+     * For a labeled diagram of the shooter, see: 
+     * https://bit.ly/frc7153_2022livewire
      */
 }
